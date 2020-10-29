1c881fe1276279ac79f62d12c3ebb8ebc480e4be
c7021101 <obmann.daniel@live.de>
2020-10-29 09:34:41 +0100

    UPDATE: correctify loss functions and stop confusion with min-max game

--- train.py
+++ train.py
@@ -12,6 +12,10 @@ y_train = train[1]
 onehot = np.zeros((y_train.size, y_train.max()+1))
 onehot[np.arange(y_train.size), y_train] = 1
 
+latent = 49
+img_size = (28, 28, 1)
+classes = 10
+
 
 def conv_layer(inp, filters, kernel_size=(3, 3), dropout=0, transpose=False, **kwargs):
     out = tf.keras.layers.Conv2D(filters, kernel_size=kernel_size, padding='same', **kwargs)(inp)
@@ -25,17 +29,17 @@ def conv_layer(inp, filters, kernel_size=(3, 3), dropout=0, transpose=False, **k
 
 # Define the generator
 def get_generator(lat_dim=49, class_dim=10, filters=32):
-    zinp = tf.keras.layers.Input((lat_dim,))
+    latent_input = tf.keras.layers.Input((lat_dim,))
     m = int(np.sqrt(lat_dim))
-    z = tf.keras.layers.Reshape((m, m, 1))(zinp)
+    z = tf.keras.layers.Reshape((m, m, 1))(latent_input)
     z = conv_layer(z, filters=filters)
     z = conv_layer(z, filters=filters)
     z = conv_layer(z, filters=filters, transpose=True, kernel_size=(2, 2), strides=(2, 2))
     z = conv_layer(z, filters=filters, transpose=True, kernel_size=(2, 2), strides=(2, 2))
     z = conv_layer(z, filters=filters)
 
-    yinp = tf.keras.layers.Input((class_dim, ))
-    y = tf.keras.layers.Dense(lat_dim)(yinp)
+    label_input = tf.keras.layers.Input((class_dim, ))
+    y = tf.keras.layers.Dense(lat_dim)(label_input)
     y = tf.keras.layers.Reshape((m, m, 1))(y)
     y = conv_layer(y, filters=filters)
     y = conv_layer(y, filters=filters, transpose=True, kernel_size=(2, 2), strides=(2, 2))
@@ -47,23 +51,23 @@ def get_generator(lat_dim=49, class_dim=10, filters=32):
     out = conv_layer(out, filters=filters)
     out = tf.keras.layers.Conv2D(1, kernel_size=(3, 3), padding='same', activation='relu')(out)
 
-    generator = tf.keras.Model(inputs=[zinp, yinp], outputs=out)
-    return [zinp, yinp], out, generator
+    generator = tf.keras.Model(inputs=[latent_input, label_input], outputs=out)
+    return generator
 
 
 # Define discriminator
 def get_discriminator(img_dim=(28, 28, 1), class_dim=10, filters=32):
-    dginp = tf.keras.layers.Input(img_dim)
+    img_input = tf.keras.layers.Input(img_dim)
 
-    d = conv_layer(dginp, filters=filters)
+    d = conv_layer(img_input, filters=filters)
     d = conv_layer(d, filters=filters)
     d = conv_layer(d, filters=filters, strides=(2, 2))
     d = conv_layer(d, filters=filters, strides=(2, 2))
     d = tf.keras.layers.Flatten()(d)
     d = tf.keras.layers.Dense(class_dim, activation='relu')(d)
 
-    dyinp = tf.keras.layers.Input((class_dim,))
-    dy = tf.keras.layers.Dense(128, activation='relu')(dyinp)
+    label_input = tf.keras.layers.Input((class_dim,))
+    dy = tf.keras.layers.Dense(128, activation='relu')(label_input)
     dy = tf.keras.layers.Dense(128, activation='relu')(dy)
     dy = tf.keras.layers.Dense(class_dim, activation='relu')(dy)
 
@@ -71,54 +75,54 @@ def get_discriminator(img_dim=(28, 28, 1), class_dim=10, filters=32):
     out = tf.keras.layers.Dense(256, activation='relu')(out)
     out = tf.keras.layers.Dense(1, activation='sigmoid')(out)
 
-    discriminator = tf.keras.Model(inputs=[dginp, dyinp], outputs=out)
-    return [dginp, dyinp], out, discriminator
+    discriminator = tf.keras.Model(inputs=[img_input, label_input], outputs=out)
+    return discriminator
 
 
-ginp, gout, gen = get_generator(filters=32)
+gen = get_generator(filters=32)
 gvars = gen.trainable_variables
 
-dinp, dout, disc = get_discriminator(filters=16)
+disc = get_discriminator(filters=16)
 dvars = disc.trainable_variables
 
 
 # Discriminator tries to map x_true to output 1 and generator_out to output 0
 # Generator tries to find images such that discriminator thinks they are output 1
-LAM = 10
-z = tf.placeholder(tf.float32, (None, 49))
-x_true = tf.placeholder(tf.float32, (None, 28, 28, 1))
-x_gen = tf.placeholder(tf.float32, (None, 28, 28, 1))
-x_line = tf.placeholder(tf.float32, (None, 28, 28, 1))
-label = tf.placeholder(tf.float32, (None, 10))
+LAM = 1e0
+z = tf.placeholder(tf.float32, (None, latent))
+label = tf.placeholder(tf.float32, (None, classes))
+x_true = tf.placeholder(tf.float32, (None, ) + img_size)
+line = tf.placeholder(tf.float32, (None, 1, 1, 1))
+
+x_gen = gen([z, label])
+x_line = x_true*line + x_gen*(1-line)
+
 
 lip = LAM*sum([(tf.reduce_mean(g**2)-1)**2 for g in tf.gradients(disc([x_line, label]), [x_line, label])])
-loss = tf.reduce_mean(disc([x_true, label]) - disc([x_gen, label]) + lip)
-loss_gen = -tf.reduce_mean(disc([gen([z, label]), label]))
 
-lr = 1e-4
+loss_disc = tf.reduce_mean(disc([x_gen, label]) - disc([x_true, label]) + lip)
+loss_gen = -tf.reduce_mean(disc([x_gen, label]))
+
+lr = 1e-5
 opt = tf.train.AdamOptimizer(learning_rate=lr)
 
 train_gen = opt.minimize(loss_gen, var_list=gvars)
-train_disc = opt.minimize(-loss, var_list=dvars)
+train_disc = opt.minimize(loss_disc, var_list=dvars)
 
 
 def get_disc_batch(batch_size, latent=49, N=60000):
     idx = np.random.choice(N, batch_size)
     lat = np.random.normal(0, 1, (batch_size, latent))
     x_r, lab = x_train[idx, ...], onehot[idx, ...]
-    x_g = sess.run(gen([z, label]), feed_dict={z: lat, label: lab})
     t = np.random.uniform(0, 1, (batch_size, 1, 1, 1))
-    x_l = t*x_r + (1 - t)*x_g
-    fd = {x_true: x_r, x_gen: x_g, x_line: x_l, label: lab}
-    return fd
+    return {x_true: x_r, z: lat, label: lab, line: t}
 
 
 def get_gen_batch(batch_size, latent=49, N=60000):
     idx = np.random.choice(N, batch_size)
     lat = np.random.normal(0, 1, (batch_size, latent))
     lab = onehot[idx, ...]
-    fd = {z: lat, label: lab}
-    return fd
+    return {z: lat, label: lab}
 
 
 sess.run(tf.global_variables_initializer())
@@ -135,9 +139,9 @@ for epoch in range(epochs):
 
     for batch in range(N // batch_size):
         CRITIC = []
-        for _ in range(ncritic):
-            fd = get_disc_batch(batch_size=batch_size)
-            ld, _ = sess.run([loss, train_disc], feed_dict=fd)
+        critic_batches = [get_disc_batch(batch_size=batch_size) for i in range(ncritic)]
+        for i in range(ncritic):
+            ld, _, lipschitz = sess.run([loss_disc, train_disc, lip], feed_dict=get_disc_batch(batch_size=batch_size))
             CRITIC.append(ld)
 
         fd = get_gen_batch(batch_size=batch_size)
@@ -145,9 +149,9 @@ for epoch in range(epochs):
 
         LOSSG.append(lg)
         LOSSD.append(np.mean(CRITIC))
-        print("Loss %f, %f" % (np.mean(LOSSD), np.mean(LOSSG)), end='\r')
+        print("Progress: %f, Disc: %f, Gen: %f, Lipschitz: %f" % ((batch*batch_size)/N, np.mean(LOSSD), np.mean(LOSSG), lipschitz), end='\r')
 
-    lat = np.random.normal(0, 1, (4, 49))
+    lat = np.random.normal(0, 1, (4, latent))
     lab = onehot[np.random.choice(N, 4), ...]
 
     gx = sess.run(gen([z, label]), feed_dict={z: lat, label: lab})
